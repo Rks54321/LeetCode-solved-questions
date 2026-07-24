a361d558dbef0323d30e7ba063d40454deedb6fc
RAKESH KUMAR SAHOO <rakeshkumar756131@gmail.com>
2026-07-24 12:30:16 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.28%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,22 +15,23 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> leftHeight = new LinkedList<>();
-        Queue<TreeNode> rightHeight = new LinkedList<>();
-        leftHeight.add(p);
-        rightHeight.add(q);
+        Queue<TreeNode> leftElementQueue = new LinkedList<>();
+        Queue<TreeNode> rightElementQueue = new LinkedList<>();
 
-        while(!leftHeight.isEmpty() && !rightHeight.isEmpty()) {
-            TreeNode leftElement = leftHeight.poll();
-            TreeNode rightElement = rightHeight.poll();
+        leftElementQueue.add(p);
+        rightElementQueue.add(q);
+
+        while(!leftElementQueue.isEmpty() && !rightElementQueue.isEmpty()) {
+            TreeNode leftElement = leftElementQueue.poll();
+            TreeNode rightElement = rightElementQueue.poll();
             if(leftElement == null && rightElement == null) continue;
             if(leftElement == null || rightElement == null || leftElement.val != rightElement.val) return false;
 
-            leftHeight.add(leftElement.left);
-            rightHeight.add(rightElement.left);
+            leftElementQueue.add(leftElement.left);
+            rightElementQueue.add(rightElement.left);
 
-            leftHeight.add(leftElement.right);
-            rightHeight.add(rightElement.right);
+            leftElementQueue.add(leftElement.right);
+            rightElementQueue.add(rightElement.right);
         }
         return true;
     }
