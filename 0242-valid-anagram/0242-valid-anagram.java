b02fd419568ec595ec86be508a0fb3998e2f6474
RAKESH KUMAR SAHOO <rakeshkumar756131@gmail.com>
2026-07-31 22:25:54 +0530

    Time: 2 ms (99.62%), Space: 44.8 MB (55.14%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[256];
+        for(char c : s.toCharArray()) arr[c - 'a']++;
+        for(char c : t.toCharArray()) arr[c - 'a']--;
+        for(int i : arr) {
+            if(i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
