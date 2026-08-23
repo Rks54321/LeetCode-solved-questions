557f3383d436923370b6833d037eb16b31dc80c6
RAKESH KUMAR SAHOO <rakeshkumar756131@gmail.com>
2026-08-23 22:44:03 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.91%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,6 +6,7 @@ class Solution:
         i = m-1
         j = n-1
         k = m+n-1
+
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -18,5 +19,3 @@ class Solution:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
-        
-        
